2f104d27532cc8362a844e3224b5cd48679d5cac
zxc10110 <kannyasaenkham@gmail.com>
2022-11-13 20:06:20 +0700

    first commit

---
+++ src/java/myservlet/StudentApp.java
@@ -0,0 +1,128 @@
+//63050096 กั
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DecimalFormat;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author kanya
+ */
+public class StudentApp extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        //input
+        String id = request.getParameter("id");
+        String name = request.getParameter("name");
+        String gpa = request.getParameter("gpa");
+        double gpaDouble = Double.parseDouble(gpa);
+        
+        //condition
+        List<Student> studList = StudentTable.findAllStudent();
+        try {
+           
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet FtoC</title>"); 
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1> ID = " + id + "</h1>");
+            out.println("<h1> Name = "+ name + "</h1>");
+            out.println("<h1> GPA =" + gpa + "</h1>");
+            //printAllStudent(studList);
+            out.println("</body>");
+            out.println("</html>");
+            
+        } finally {            
+            out.close();
+        }
+    }
+     
+    public static void printAllStudent(List<Student> studentList) {
+        for(Student emp : studentList) {
+           System.out.print(emp.getId() + " ");
+           System.out.print(emp.getName() + " ");
+           System.out.println(emp.getGpa() + " ");
+       }
+    }
+    
+    public static void persist(Object object) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("StudentApp");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.persist(object);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
